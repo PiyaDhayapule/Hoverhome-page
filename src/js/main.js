967d8a92d76ca6aea8798ca36bf4e58a15447e3e
priya <priyadhayapule99@gmail.com>
2019-01-12 14:04:27 +0530

    Testing.

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// border animation for Nav items
+
+$(document).ready(function(){
+  $('.nav li a').addClass('border-animation');
+});
+
+$(document).ready(function(){
+    $('.nav-nav li a').addClass('border-animation-1');
+  });
+
+$(document).ready(function(){
+    $('.user-nav li a').addClass('border-animation-2');
+  });
